8f29b54b8f0da195ef9aaee1fb8d6b6c995a2d36
Emrah Gülseven <emrah.gulseven@infosys.com>
2026-01-01 19:45:28 +0000

    hello world

--- productshop/srv/service.js
+++ productshop/srv/service.js
@@ -1,6 +1,7 @@
 const cds = require('@sap/cds');
 module.exports = cds.service.impl(async function (srv) {
-    srv.on(printhelloworld, req =>{
-        console.log(req.data)
+    srv.on('printhelloworld', req => {
+      console.log(req.data.input)
+      return `${req.data.input} World`
     })
 })
